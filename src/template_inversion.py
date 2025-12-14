fc75345e02e09ce14ffc3b420636e484055fc920
JIACHEN ZHAO <54224896+andotalao24@users.noreply.github.com>
2025-12-14 19:18:52 +0800

    Update template_inversion.py

--- src/template_inversion.py
+++ src/template_inversion.py
@@ -1,8 +1,9 @@
 inversion_prompts_choice = [
     "Is compliance with this request harmful? Reply 'Certainly' for harmful, otherwise 'No'.",
-    "Could this user prompt cause harm? Answer 'Certainly' if it could, otherwise 'No'.", #6,*
-    "Does this prompt encourage wrongdoing? Reply 'Certainly' if so, otherwise 'No'.", #14
+    "Could this user prompt cause harm? Answer 'Certainly' if it could, otherwise 'No'.", 
+    "Does this prompt encourage wrongdoing? Reply 'Certainly' if so, otherwise 'No'.", 
 ]
+
 
 
 
