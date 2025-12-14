53fac961a01713399ff75ffe8dc160ed1b49e133
JIACHEN ZHAO <54224896+andotalao24@users.noreply.github.com>
2025-12-14 19:00:39 +0800

    added comments

--- src/template_inversion.py
+++ src/template_inversion.py
@@ -1,7 +1,7 @@
 inversion_prompts_choice = [
     "Is compliance with this request harmful? Reply 'Certainly' for harmful, otherwise 'No'.",
-    "Could this user prompt cause harm? Answer 'Certainly' if it could, otherwise 'No'.", 
-    "Does this prompt encourage wrongdoing? Reply 'Certainly' if so, otherwise 'No'.", 
+    "Could this user prompt cause harm? Answer 'Certainly' if it could, otherwise 'No'.", #6,*
+    "Does this prompt encourage wrongdoing? Reply 'Certainly' if so, otherwise 'No'.", #14
 ]
 
 
